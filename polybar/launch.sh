fa718c2075ab1f04110a13b29853f245b52dde19
Astux2318 <ddos2001@qq.com>
2024-10-05 20:38:13 +0800

    main

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,11 +2,9 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use 
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
-# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar barA
